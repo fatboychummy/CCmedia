843adb6399cd950762e6d45088c18c1472bdad9d
Fatboychummy <39929480+fatboychummy@users.noreply.github.com>
2019-04-09 22:16:03 -0600

    update location to the raw location

--- helpfulthings/FatInventoryHandler.lua
+++ helpfulthings/FatInventoryHandler.lua
@@ -1,6 +1,6 @@
 --[[
 2
-https://github.com/fatboychummy/CCmedia/blob/master/helpfulthings/FatInventoryHandler.lua
+https://raw.githubusercontent.com/fatboychummy/CCmedia/blob/master/helpfulthings/FatInventoryHandler.lua
 ]]
 
 local meta = {}
