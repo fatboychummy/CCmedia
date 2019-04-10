6d5415e7dd8f684f91a8ba93660ad2f717ce815e
Fatboychummy <39929480+fatboychummy@users.noreply.github.com>
2019-04-09 21:56:07 -0600

    change location to master

--- helpfulthings/FatFileUpdateHandler.lua
+++ helpfulthings/FatFileUpdateHandler.lua
@@ -1,6 +1,6 @@
 --[[FATFILE
 2
-https://github.com/fatboychummy/CCmedia/blob/updateTesting/helpfulthings/FatFileUpdateHandler.lua
+https://github.com/fatboychummy/CCmedia/blob/master/helpfulthings/FatFileUpdateHandler.lua
 ]]
 
 if not fs.exists("/FatFileSystem.lua") then
