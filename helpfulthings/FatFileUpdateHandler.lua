ce98f8c1bd96ac8c83b24448426544ab879bf4b4
Matthew W <fatboychummy@gmail.com>
2019-05-18 01:13:07 -0600

    yeet

--- helpfulthings/FatFileUpdateHandler.lua
+++ helpfulthings/FatFileUpdateHandler.lua
@@ -24,12 +24,11 @@ if needLoad then
   end
 end
 
-local bassert = type(fe) == "table" and type(fe.bassert) == "function" and fe.bassert 
+local bassert = type(fe) == "table" and type(fe.bassert) == "function" and fe.bassert
               or function(a, ...) if not a then error(table.concat({...}, " "), 3) end end
 fe = nil
 
-
-local function updateFile(fileInfo)
+local function updateCheck(fileInfo)
   bassert(type(fileInfo) == "table", 2, "updateFile", 1, "table", fileInfo)
 
   local version = false
@@ -40,41 +39,52 @@ local function updateFile(fileInfo)
     return false, "Missing version information."
   end
   if fileInfo["location"] then
-    if fileInfo["file"] then
-      local h = http.get(fileInfo.location)
-      if h then
-        local open = h.readLine()
-        local verLine = h.readLine()
-        local ver = tonumber(verLine)
-        if version < ver then
-          local h2 = fs.open(fileInfo.file, "w")
-          if h2 then
-            h2.writeLine(open)
-            h2.writeLine(verLine)
-            h2.write(h.readAll())
-            h.close()
-            h2.close()
-            return true
-          else
-            h.close()
-            return false, "Failed to open file for writing ("
-              .. tostring(fileInfo.file) .. ")."
-          end
-        else
-          h.close()
-        end
-      else
-        return false, "Failed to open http handle ( "
-          .. tostring(fileInfo.location) .. ") for file \""
-          .. tostring(fileInfo.file) .. "\"."
+    local h = http.get(fileInfo.location)
+    if h then
+      local open = h.readLine()
+      local verLine = h.readLine()
+      local ver = tonumber(verLine)
+      h.close()
+      if version < ver then
+        return true
       end
     else
-      return false, "Missing file location."
+      error("Failed to open http handle ( "
+        .. tostring(fileInfo.location) .. ") for file \""
+        .. tostring(fileInfo.file) .. "\". (update-check)")
     end
   else
     return false, "Missing http location information."
   end
   return false, "No update required."
+end
+
+local function updateFile(fileInfo)
+  local needUpdate = updateCheck(fileInfo)
+  if needUpdate then
+    if fileInfo["file"] then
+      local h = http.get(fileInfo.location)
+      if h then
+        local h2 = io.open(fileInfo.file, "w")
+        if h2 then
+          h2:write(h.readAll()):close()
+          h.close()
+          return true
+        else
+          h.close()
+          error("Failed to open file for writing ("
+                .. tostring(fileInfo.file) .. ")")
+        end
+      else
+        error("Failed to open http handle ( "
+              .. tostring(fileInfo.location) .. ") for file \""
+              .. tostring(fileInfo.file) .. "\". (write)")
+    else
+      error("Missing file location information.")
+    end
+  else
+    return false, "No update required."
+  end
 end
 
 local function updateAllFATS()
@@ -93,7 +103,7 @@ local function updateAllFATS()
   print("Updated all files.")
 end
 
-  
+
 if needLoad then
   return {updateFile = updateFile}
 else
